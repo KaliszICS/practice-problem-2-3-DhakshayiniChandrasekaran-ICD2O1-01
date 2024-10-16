4cc3badd725ee85590d3de19d711717cf91f1bb4
DhakshayiniChandrasekaran-ICD2O1-01 <dhakshayini.chandrasekaran@student.tdsb.on.ca>
2024-10-16 17:23:33 +0000

    e

--- PP2_3.py
+++ PP2_3.py
@@ -32,7 +32,7 @@ def q3():
   side1 = float(input("Input a number: "))
   side2 = float(input("Input a number: "))
   side3 = float(input("Input a number: "))
-  if side1+side2>side3 or side1+side3>side2 or side2+side3>side1:
+  if side1+side2>side3 and side1+side3>side2 and side2+side3>side1:
     if side1==side2==side3:
       print("Equilateral")
     elif side1 == side2 or side1==side3 or side2==side3:
